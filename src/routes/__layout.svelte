50b4e0d4ea8f4858cdfb413c7dfdf5d868e79f58
MatejGroombridge <matejdpg@gmail.com>
2021-12-14 20:46:39 +1100

    moved gtag to __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,20 @@
 	import '../css/app.css';
 </script>
 
+<svelte:head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-LK1M7GLR61"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-LK1M7GLR61');
+	</script>
+</svelte:head>
+
 <Header />
 
 <main>
