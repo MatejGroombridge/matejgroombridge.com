b8edb31b0fb6672eb718f73fcf254ce6cf01294e
MatejGroombridge <matejdpg@gmail.com>
2021-12-27 10:33:25 +1100

    removed books.ts contents

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -1,9 +1,9 @@
-import type { EndpointOutput } from '@sveltejs/kit';
+// import type { EndpointOutput } from '@sveltejs/kit';
 
-export async function get(): Promise<EndpointOutput> {
-    const res = await fetch('http://localhost:1337/books');
-    const data = await res.json();
-    console.log("hiasdflkaj")
+// export async function get(): Promise<EndpointOutput> {
+//     const res = await fetch('http://localhost:1337/books');
+//     const data = await res.json();
+//     console.log("hiasdflkaj")
     
-    return { body: data };
-}
+//     return { body: data };
+// }
