328ef9318230df2a35550f4ffedae5372baf409c
MatejGroombridge <matejdpg@gmail.com>
2023-01-27 16:37:51 +1100

    fixed year 9 form value

--- src/lib/contact/WaitingListContact.svelte
+++ src/lib/contact/WaitingListContact.svelte
@@ -64,7 +64,7 @@
 				<option value="6">Year 6</option>
 				<option value="7">Year 7</option>
 				<option value="8">Year 8</option>
-				<option value="8">Year 9</option>
+				<option value="9">Year 9</option>
 			</select>
 		</div>
 
