be8ba850cf024b7517d874180552bf1bfbb9b50e
MatejGroombridge <matejdpg@gmail.com>
2023-02-12 13:56:51 +1100

    testing individual booknotes pages to fix errors

---
+++ src/routes/booknotes/dohardthings.svelte
@@ -0,0 +1,15 @@
+<script>
+	import BookSlug from '$lib/booknotes/BookSlug.svelte';
+
+	const ID = 'dohardthings';
+	const name = 'Do Hard Things';
+	const spiel =
+		'Teenagers are capable of doing amazing things but fail to reach the full potential because of the low expectations that society puts on us. Youth is the molding stage of men so what we embody today reflects what we will become as adults. Doing hard things in your teen years prepares your for a life of incredible impact.';
+</script>
+
+<svelte:head>
+	<title>{name} | Summary, Notes & Quotes</title>
+	<meta name="description" content={spiel} />
+</svelte:head>
+
+<BookSlug {ID} />
