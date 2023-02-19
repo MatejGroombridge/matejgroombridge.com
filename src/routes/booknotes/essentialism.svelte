ac4674e64b55aadc83b8b435ad4b443fd82d5f34
MatejGroombridge <matejdpg@gmail.com>
2023-02-19 14:41:29 +1100

    fixed essentialism

--- src/routes/booknotes/essentialism.svelte
+++ src/routes/booknotes/essentialism.svelte
@@ -4,7 +4,7 @@
 	const ID = 'essentialism';
 	const name = 'Essentialism';
 	const spiel =
-		'Essentialism means doing less, better. So many people suffer from overcommitment and saying yes to everything.By only doing what is essential you gain time, clarity and fulfillment.';
+		'Essentialism means doing less, better. So many people suffer from overcommitment and saying yes to everything. By only doing what is essential you gain time, clarity and fulfillment.';
 </script>
 
 <svelte:head>
