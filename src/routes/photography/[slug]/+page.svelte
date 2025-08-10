56e79c5eeb66bdaf3f8b9648c38ac3c132de783c
MatejGroombridge <matejdpg@gmail.com>
2025-08-10 22:46:05 +1000

    fixed photography modal not showing on desktop

--- src/routes/photography/[slug]/+page.svelte
+++ src/routes/photography/[slug]/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+	// @ts-nocheck
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 	import Gallery from '$lib/photography/Gallery.svelte';
 	import { tripSlugs } from '$lib/photography/trip-slugs.js';
 	import { photosData } from '$lib/photography/photos-data.js';
@@ -24,6 +26,7 @@
 
 	const ID = $page.params.slug;
 	let length = 0;
+	let isDesktop = false;
 
 	// Images are served from the static directory; construct URLs directly.
 
@@ -47,6 +50,21 @@
 	}
 
 	getLength();
+
+	// Enable modal only on desktop (hover + fine pointer + reasonable width)
+	onMount(() => {
+		const mq = window.matchMedia('(hover: hover) and (pointer: fine) and (min-width: 768px)');
+		const update = () => (isDesktop = mq.matches);
+		update();
+		if (mq.addEventListener) {
+			mq.addEventListener('change', update);
+			return () => mq.removeEventListener('change', update);
+		} else {
+			// Safari fallback
+			mq.addListener(update);
+			return () => mq.removeListener(update);
+		}
+	});
 </script>
 
 <svelte:head>
@@ -82,7 +100,7 @@
 	</div>
 	<div class="two-margin mobile-two-margin"></div>
 	<div class="wrapper shown">
-		<Gallery gap={20} maxColumnWidth={300} hasModal={false} type="tripImages">
+		<Gallery gap={20} maxColumnWidth={300} hasModal={isDesktop} type="tripImages">
 			{#each Array.from({ length }).keys() as photoNum}
 				<img
 					src={`/photography/all-photos/${ID + '_' + photoNum}.webp`}
